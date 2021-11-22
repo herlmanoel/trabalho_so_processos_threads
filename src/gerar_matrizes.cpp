83081390fbcad9d6229c0328e9cd9eee088a586d
herlmanoel <herlmanoel@herlmanoel.localdomain>
2021-11-22 19:56:07 -0300

    org arquivos

--- src/gerar_matrizes.cpp
+++ src/gerar_matrizes.cpp
@@ -16,13 +16,14 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
+    int N = atoi(argv[1]);
+    
     Matriz* m1 = new Matriz();
     Matriz* m2 = new Matriz();
 
 
-    m1->gerarMatrizArquivo(6, 6, "data/input/arquivo01.txt");
-    m2->gerarMatrizArquivo(6, 6, "data/input/arquivo02.txt");
+    m1->gerarMatrizArquivo(N, N, "data/input/arquivo01.txt");
+    m2->gerarMatrizArquivo(N, N, "data/input/arquivo02.txt");
 
     
     return 0;
